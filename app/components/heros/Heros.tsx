62141e8d3563ae716fc50fbf469c661539c95b69
Vientousky <xxmancuellojosexx@gmail.com>
2025-03-06 09:46:35 -0300

    v6

--- app/components/heros/Heros.tsx
+++ app/components/heros/Heros.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./home.module.css";
+import Link from "next/link";
 
 const HomeHero: React.FC = () => {
   return (
     <section className={styles.hero} id="heros">
       <div className={styles.container}>
         <h1>
-          ¡Bievenido a la{" "}
-          <strong className="text-gradient">ESS N59!</strong>
+          ¡Bievenido a la <strong className="text-gradient">ESS N59!</strong>
         </h1>
 
         <p>
@@ -16,9 +16,11 @@ const HomeHero: React.FC = () => {
         </p>
 
         <div className={styles.btn}>
-          <a href="">Ver Reglamento</a>
+          <Link className={styles.ir}  href="/reglamento">Ver Reglamento</Link>
 
-          <a href="">Descargar</a>
+          <a className={styles.descargar} href="">
+            Descargar
+          </a>
         </div>
       </div>
     </section>
