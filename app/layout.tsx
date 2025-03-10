68e902d2c3cc9bdaa6415fa5782546fe47415ece
Vientousky <jotamancuello2048819@gmail.com>
2025-03-10 08:50:12 -0300

    v7.3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Header from "./layout/header/Header";
 import Footer from "./layout/footer/Foter";
 import "./ui/globals.css";
@@ -20,10 +22,10 @@ export default function RootLayout({
     <html lang="es">
       <body>
         <Header/>
-
         {children}
-
         <Footer/>
+        <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
